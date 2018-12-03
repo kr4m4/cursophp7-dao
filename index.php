427bcf4120acf7472ddfede30e89c1959d88e7a6
Kr4m4 <fbi_509@hotmail.com>
2018-12-03 11:55:17 -0200

    metodos de lista

--- index.php
+++ index.php
@@ -1,17 +1,26 @@
 <?php
 
 require_once("config.php");
+//carrega um usuario
+//$usuario = new Usuario();
+//$usuario->loadById(1);
+//echo $usuario;
 
+//$sql = new Sql();
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//echo json_encode($usuarios);
+
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//carrega um usuario usando o login e senha
 $usuario = new Usuario();
-
-$usuario->loadById(1);
+$usuario->login("joao", "qwert");
 
 echo $usuario;
-
-/*$sql = new Sql();
-
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);*/
-
 ?>
